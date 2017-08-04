44f2461b9a2f4e827320b68afdfe2fe0a3f627af
pfeodrippe <pfeodrippe@gmail.com>
2017-08-03 23:37:26 -0300

    add macro to help with push-matrix

--- src/quil_test_01/core.clj
+++ src/quil_test_01/core.clj
@@ -2,6 +2,16 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+;; example from https://twitter.com/beesandbombs
+;; https://gist.github.com/anonymous/9f7e57ab4ec7d8073e545e36db1ecf64
+
+(defmacro with-matrix
+  [& body]
+  `(do
+     (q/push-matrix)
+     ~@body
+     (q/pop-matrix)))
+
 
 (def samples-per-frame 8)
 
@@ -17,21 +27,14 @@
   (q/smooth 8)
   (q/fill 32)
   (q/no-stroke)
-  {:color 0
-   :angle 0
-   :radius 0})
+  {})
 
 
 (defn update-state [state]
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)
-   :radius (mod (+ (:radius state) 1) 100)})
+  {})
 
 
 (defn draw-state [state]
-  (q/background 250)
-  ;(q/fill (:color state) 255 255)
-
   (doseq [sa (range 0 samples-per-frame)]
     (let [t (q/map-range (+ (dec (q/frame-count))
                             (-> sa (* shutter-angle) (/ samples-per-frame)))
@@ -39,36 +42,27 @@
                          num-frames
                          0
                          1)]
-      (do
-        (q/background 250)
-        (q/push-matrix)
+      (q/background 250)
+      (with-matrix
         (q/translate (/ (q/width) 2) 160)
         (doseq [i (range 0 N)]
           (let [l (q/map-range i -0.75 (dec N) 0 320)]
-            (do
-              (q/stroke 32)
-              (q/no-fill)
-              (q/arc 0 0 (* 2 l) (* 2 l) 0 Math/PI)
-              (q/fill 32)
-              (q/no-stroke)
-              (q/push-matrix)
-              (q/rotate (-> t (* (- N i)) (* 0.5 Math/PI) Math/sin (* (/ Math/PI 2))))
+            (q/stroke 32)
+            (q/no-fill)
+            (q/arc 0 0 (* 2 l) (* 2 l) 0 q/PI)
+            (q/fill 32)
+            (q/no-stroke)
+            (with-matrix
+              (q/rotate (-> t (* (- N i)) (* q/TWO-PI) q/sin (* q/HALF-PI)))
               (q/translate 0 l)
-              (q/ellipse 0 0 14 14)
-              (q/pop-matrix))))
-        (q/pop-matrix)))))
+              (q/ellipse 0 0 14 14))))))))
 
 
 (q/defsketch quil-test-01
   :title "Bee and Bombs - Dots"
   :size [700 500]
-  ; setup function called only once, during sketch initialization.
   :setup setup
-  ; update-state is called on each iteration before draw-state.
   :update update-state
   :draw draw-state
   :features [:keep-on-top]
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
   :middleware [m/fun-mode])
